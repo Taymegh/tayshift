7d3636476d5a31e38e2017fa80d24eff18d34e3a
Debian <debian@vps-8596a54d.vps.ovh.net>
2024-10-27 20:28:24 +0000

    Ajout des données dans le json

--- success.php
+++ success.php
@@ -1,11 +1,38 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
+$user = $_POST['user'];
+$usermail = $_POST['mail'];
+$pass= $_POST['passwd'];
+
+
+
+$file = 'data.json';
+
+$newData = array(
+    'login' => $user,
+    'email' => $usermail,
+    'password' => $pass
+);
+
+if (file_exists($file)) {
+    $currentData = json_decode(file_get_contents($file), true);
+    if (!$currentData) {
+        $currentData = array(); 
+    }
+} else {
+    $currentData = array(); 
+}
+
+
+$currentData[] = $newData;
+
+file_put_contents($file, json_encode($currentData, JSON_PRETTY_PRINT));
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
@@ -23,13 +50,31 @@ $mail->Port = 465;
 
 $mail->setFrom('tayshift25@gmail.com');
 $mail->addAddress($_POST['mail']);
+$mail->isHTML(true);
 
-$mail->Subject = 'Test';
-$mail->Body = 'Tu as recu un mail!';
+$mail->Subject = 'Demande de creation de compte';
+
+$mail->Body = "
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nous avons bien reçu votre demande de compte.</h1>
+        <h2>Récapitulatif de votre demande:</h2>
+    <ul>
+        <li>Login: $user</li>
+        <li>Email: $usermail</li>
+        <li>Mot de passe: $pass</li>
+    </ul>
+</body>
+</html> 
+";
 
 $mail->send();
 
 ?>
+
 
 
 
@@ -44,11 +89,11 @@ $mail->send();
 <body>
     <h1>Demande de compte envoyée!</h1>
     <h2>Un mail de confirmation vous a été envoyé à: <?php echo $_POST['mail']; ?></h2>
-    <h2>Récapitulatif de votre demande:</h2>
+    <!-- <h2>Récapitulatif de votre demande:</h2>
     <ul>
         <li>Login:<?php echo $_POST['user']; ?></li>
         <li>Email:<?php echo $_POST['mail']; ?></li>
         <li>Mot de passe:<?php echo $_POST['passwd']; ?></li>
-    </ul>
+    </ul> -->
 </body>
 </html>
