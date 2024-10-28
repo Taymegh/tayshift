b6759287a92bde2760046660ebec5796a61d295e
Samuel Delabranche <samuel.delabranche0@gmail.com>
2024-10-28 12:07:22 +0100

    dataBase

---
+++ Data/dataBase.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Tayshiftdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error)
+{
+ die("La connexion à la base de données a échoué : " . $conn->connect_error);
+} 
+?>
---
+++ Home/index.php
@@ -0,0 +1,24 @@
+<?php
+    include("../Data/dataBase.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tayshift</title>
+    <link rel="icon" type="image/x-icon" href="icone.ico">
+    <script src="https://kit.fontawesome.com/4002a2cc72.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Style/index.css">
+</head>
+<body>
+    <img src="../Assets/Img/Tayshift.png">
+    <h2>Transferez vos fichiers, ou que vous soyez.</h2>
+
+    <div class="buttons">
+        <button onclick="">Se connecter</button>
+        <Button onclick="">Creer un compte</Button>
+    </div>
+</body>
+</html>
