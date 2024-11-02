b1f66f69c29b5b69afcf2e82e9348c03d68eaecf
Samuel Delabranche <samuel.delabranche0@gmail.com>
2024-11-02 11:36:45 +0100

    Mise en place de la première page

--- Home/index.php
+++ Home/index.php
@@ -1,32 +1,46 @@
+
 <?php
     include("../Data/connectDataBase.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Tayshift</title>
 
-    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.css">
-    <link rel="icon" type="image/x-icon" href="icone.ico">
-    <script src="https://kit.fontawesome.com/4002a2cc72.js" crossorigin="anonymous"></script>
+    <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="Style/index.css">
+
 </head>
 <body>
+  <div class="container">
+      <div class="login-form text-center">
+          <img src="../Assets/Img/logo.ico" alt="TAYSHIFT Logo">
+          
+          <div class="card-body">
+              <h5 class="card-title">TAYSHIFT</h5>
+              <p class="card-text">Transférez vos fichiers, où que vous soyez.</p>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="../Assets/Img/logo.ico" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+              <form>
+                  <div class="mb-3">
+                      <label for="username" class="form-label visually-hidden">Nom d'utilisateur</label>
+                      <input type="text" class="form-control" id="username" placeholder="Nom d'utilisateur">
+                  </div>
+                  <div class="mb-3">
+                      <label for="password" class="form-label visually-hidden">Mot de passe</label>
+                      <input type="password" class="form-control" id="password" placeholder="Mot de passe">
+                  </div>
+                  <button type="submit" class="btn btn-primary w-100">Se connecter</button>
+              </form>
+
+              <div class="mt-3">
+                  <a href="#">Créer un compte</a> / <a href="#">Mot de passe oublié</a>
+              </div>
+          </div>
+      </div>
   </div>
-</div>
-
-    <script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.js"></script>
+<script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
 </html>
