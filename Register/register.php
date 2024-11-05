24482ea183c7192360993693cf3dff98d500db97
Taymegh <tmeghiche@gmail.com>
2024-11-05 10:40:51 +0100

    Page de création de compte

--- Register/register.php
+++ Register/register.php
@@ -1,8 +1,3 @@
-
-<?php
-    include("../Data/connectDataBase.php");
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,7 +6,7 @@
     <title>Tayshift</title>
 
     <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="Style/index.css">
+    <link rel="stylesheet" href="Style/register.css">
 
 </head>
 <body>
@@ -23,19 +18,28 @@
               <p class="card-text CaramelFont">Transferez vos fichiers, ou que vous soyez.</p>
 
               <form>
-                  <div class="mb-3">
-                      <label for="username" class="form-label visually-hidden">Nom d'utilisateur</label>
-                      <input type="text" class="form-control" id="username" placeholder="Nom d'utilisateur">
-                  </div>
+                <div class="row mb-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="username" class="form-label visually-hidden">Nom d'utilisateur</label>
+                        <input type="text" class="form-control" id="username" placeholder="Nom d'utilisateur">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="email" class="form-label visually-hidden">Adresse mail</label>
+                        <input type="text" class="form-control" id="email" placeholder="Adresse mail">
+                    </div>
+                </div>
+
+
                   <div class="mb-3">
                       <label for="password" class="form-label visually-hidden">Mot de passe</label>
                       <input type="password" class="form-control" id="password" placeholder="Mot de passe">
                   </div>
-                  <button type="submit" class="btn btn-primary w-100">Se connecter</button>
+
+                  <button type="submit" class="btn btn-primary w-100">Créer compte</button>
               </form>
 
               <div class="mt-3">
-                  <a href="../Register/register.php">Créer un compte</a> / <a href="#">Mot de passe oublié</a>
+                  <a href="#">Accueil</a>
               </div>
           </div>
       </div>
