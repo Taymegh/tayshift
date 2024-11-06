3c2e4d97c0badab4a9f175191908f7a9ebdb71b3
Taymegh <tmeghiche@gmail.com>
2024-11-06 16:10:51 +0100

    Page de réinitialisation du mot de passe

---
+++ Reset/reset.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tayshift</title>
+
+    <link href="../bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="Style/reset.css">
+
+</head>
+<body>
+  <div class="container">
+      <div class="login-form text-center">
+          <img src="../Assets/Img/logo.ico" alt="TAYSHIFT Logo">
+          
+          <div class="card-body">
+              <p class="card-text CaramelFont">Transferez vos fichiers, ou que vous soyez.</p>
+              <h5 class="card-title">Vous avez oublié votre mot de passe?</h5>
+
+              <form>
+                    <div class="col-md-12 mb-3">
+                        <label for="email" class="form-label visually-hidden">Adresse mail</label>
+                        <input type="text" class="form-control" id="email" placeholder="Adresse mail">
+                    </div>
+
+                  <button type="submit" class="btn btn-primary w-100">Envoyer</button>
+              </form>
+
+              <div class="mt-3">
+                  <a href="#">Accueil</a>
+              </div>
+          </div>
+      </div>
+  </div>
+<script src="../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
