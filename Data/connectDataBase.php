c3aa4880a5c847cd7337e393e2c7d6140a1576da
subbu176 <153492494+subbu176@users.noreply.github.com>
2024-11-07 16:12:03 +0100

    Création du compte admin dans la base de données

--- Data/connectDataBase.php
+++ Data/connectDataBase.php
@@ -13,9 +13,16 @@ if (!$conn) {
 $conn->exec("CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
+    passwd TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    isAdmin INTEGER DEFAULT 0
+    isAdmin INTEGER DEFAULT 1
 )");
+
+
+$conn->exec("INSERT OR IGNORE INTO users (id, username, passwd, email, isAdmin)
+             VALUES (1, 'admin', 'User1234', 'admin@gmail.com', 0)");
+
+
 
 $conn->close();
 
